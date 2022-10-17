a9a7f9fbc4904f3545462f0d5d648c7c251164f3
Wyatt Horton <hortonwhy@protonmail.com>
2022-10-17 18:07:19 -0500

    Added footer, centered. and Basicq

---
+++ hdc-react/src/components/Footer.js
@@ -0,0 +1,13 @@
+import { Center, Flex } from "@chakra-ui/react";
+
+export const Footer = () => {
+  return (
+    <Center>
+      <Flex position="fixed" bottom="0">
+        Copyright © 2022 HDC
+      </Flex>
+    </Center>
+  );
+};
+
+export default Footer;
